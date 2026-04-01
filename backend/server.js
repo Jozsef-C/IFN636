78c97a2aff00d5ffdbd57bca641912b553e26a09
Jozsef Csonti <jcosnti2002@gmail.com>
2026-04-01 23:50:44 +1000

    Register ticket routes in backend server

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
 //app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
