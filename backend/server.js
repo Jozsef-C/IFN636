f74b15a91d50eb29ccce5a926f8f843c0b24dc52
Jozsef Csonti <jcosnti2002@gmail.com>
2026-04-02 01:20:54 +1000

    Register booking routes in backend server

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+app.use('/api/bookings', require('./routes/bookingRoutes'));
 //app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Export the app object for testing
